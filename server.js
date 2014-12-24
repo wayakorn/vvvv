7bbebfa48e37236fec99c8b2d0f34ec7e3556536
wayakorn <wayakorn@gmail.com>
2014-12-24 12:18:33 -0500

    Back to basic

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 var http = require('http');
-var router = require('router');
-var route = router();
-
-route.get('/', function(req, res) {
-	res.writeHead(200);
-	res.end('At ROOT');
-});
-route.get('/{base}', function(req, res) {
-	var base = req.params.base; // ex: if the path is /foo/bar, then base = foo
-
-	res.writeHead(80);
-	var body = 'Hello World<br>';
-	body += base;
-	res.end(body);
-});
-
-
-http.createServer(route).listen(80); // start the server on port 80
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
